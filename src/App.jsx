fb8d4027ad2a5f9689534853ef967de720844dc7
Tawkir711 <mdtawkirk711@gmail.com>
2023-10-15 23:12:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Link, useLoaderData } from "react-router-dom";
+import "./App.css";
+import CoffeeCard from "./Components/CoffeeCard";
+import { useState } from "react";
+
+function App() {
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
+  return (
+    <div className="m-20">
+      <h1 className="text-5xl text-center my-20 text-purple-600">
+        Our Popular Products: {coffees.length}
+      </h1>
+      <div className="text-center mb-12">
+        <Link to={'/addCoffee'}>
+          <button className="btn">Add Coffee page</button>
+        </Link>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4">
+        {coffees.map((coffee) => (
+          <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
